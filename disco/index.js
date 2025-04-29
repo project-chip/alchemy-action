98b8e1a3d772e8f282ac167bb7fb4c3355fafe9d
Hasty Granbery <hasty@granbery.org>
2025-04-29 13:03:15 -0700

    Attempt at provisional action

---
+++ disco/index.js
@@ -0,0 +1,18 @@
+const childProcess = require("child_process");
+const os = require("os");
+const process = require("process");
+
+function runAlchemy() {
+  const platform = os.platform();
+  const arch = os.arch();
+  const executable = `alchemy-action-${platform}-${arch}`;
+  const path = `${__dirname}/${executable}`;
+  console.log("Running " + path);
+  const out = childProcess.spawnSync(path, ['disco'], { stdio: 'inherit' });
+  process.exit(out.status);
+}
+
+console.log("Loaded module.");
+if (require.main === module) {
+  runAlchemy();
+}
